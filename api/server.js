f164ceb26d6772440bbcfd12a854eed8bb8b7f59
Justin Price <mr.price@Justins-iMac.local>
2020-02-26 14:07:15 -0500

    guided project

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
-
+const restricted = require('../auth/restricted-middleware');
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
 
@@ -12,10 +12,22 @@ server.use(express.json());
 server.use(cors());
 
 server.use('/api/auth', authRouter);
-server.use('/api/users', usersRouter);
+server.use('/api/users', restricted, checkRole('user'), usersRouter);
 
 server.get('/', (req, res) => {
   res.send("It's alive!");
 });
 
+function checkRole(role){
+  return (req, res, next) => {
+    if (
+      req.decodedToken &&
+      req.decodedToken.role &&
+      req.decodedToken.role.toLowerCase() === role) {
+      next();
+    } else {
+      res.status(403).json({you: "shall not pass"});
+    }
+  }
+}
 module.exports = server;
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const Users = require('../users/users-model.js');
-
+const {jwtSecret} = require('../config/secrets')
 // for endpoints beginning with /api/auth
 router.post('/register', (req, res) => {
   let user = req.body;
@@ -25,16 +26,31 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
         res.status(200).json({
           message: `Welcome ${user.username}!`,
+          token,
         });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
       }
     })
-    .catch(error => {
-      res.status(500).json(error);
+    .catch(({name, code, message, stack}) => {
+      res.status(500).json({name, code, message, stack});
     });
 });
+function generateToken(user){
+  const payload = {
+    subject: user.id,
+    username: user.username,
+    role: user.role || 'user'
+  };
+
+  const options = {
+    expiresIn: "1h",
+  }
+  return jwt.sign(payload, jwtSecret, options)
+}
+
 
 module.exports = router;
